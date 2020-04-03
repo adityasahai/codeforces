286a9d42aa2478c43ec5bda1b79625c1ebb1b0b5
adityasahai <39377715+adityasahai@users.noreply.github.com>
2020-04-03 14:00:31 -0700

    create wrong submissions 977

---
+++ wrong_submission_977.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+)
+
+func wrongSubstraction() int {
+	var number, iter int
+	fmt.Scanf("%d %d\n", &number, &iter)
+
+	for i := 0; i < iter; i++ {
+		if number%10 == 0 {
+			number /= 10
+		} else {
+			number--
+		}
+	}
+	return number
+}
+
+func main() {
+	fmt.Printf("%d", wrongSubstraction())
+}
